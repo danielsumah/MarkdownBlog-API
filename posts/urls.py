05b316424a34f634f61995a0bd2dfdf7ea7eac72
danielsumah <danielsumah@gmail.com>
2021-07-20 01:25:20 +0100

    post detail endpoints made

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from rest_framework.generics import RetrieveAPIView
 from .views import *
 
 app_name = 'posts'
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name="posts-url")
+    path('posts/', PostListView.as_view(), name="posts-url"),
+    path('post/<slug>/', PostDetailView.as_view(), name="get-post-url"),
 ]
