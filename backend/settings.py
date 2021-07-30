51a8084b45729e243fcffafd6cfb69bddbe8513d
danielsumah <danielsumah@gmail.com>
2021-07-30 22:05:04 +0100

    config for django

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
 
 
